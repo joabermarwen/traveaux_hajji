962e7939d516155cb3d4c2985274e1e18a80d912
joabermarwen <jaabermarwen@gmail.com>
2025-03-20 23:31:11 +0100

    add payment

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -408,7 +408,7 @@ class SiteController extends Controller
                     $deposit->payment_gateway = $request->selected_payment_gateway; // Assuming Stripe
                     $deposit->subscription_id = $buy_subscription->id;
                     $response = PaypalSdkController::process($deposit);
-                    dd($response);
+                    
                     if (!empty($response['redirect'])) {
                         return redirect($response['redirect_url']);
                     }
